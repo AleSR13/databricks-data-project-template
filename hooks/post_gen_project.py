6f3c493e756f89d337b5a55b69dcc121cf4d7853
alesure-danone <43819685+AleSR13@users.noreply.github.com>
2025-07-21 15:47:41 +0200

    [FEAT] Add pyproject.toml

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,12 +1,36 @@
+import logging
 import pathlib
 import shutil
 
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
 
-input_dir = pathlib.Path("{{cookiecutter.project_name}}")
 
-for file in input_dir.glob("**/*"):
-    relative_path = file.relative_to(input_dir)
-    relative_path.parent.mkdir(parents=True, exist_ok=True)
-    if file.is_file():
-        print(f"Copying {file} to {relative_path}")
-        shutil.copy(file, relative_path)
+def main():
+
+    project_root = pathlib.Path.cwd()
+    project_name_folder = project_root / "{{cookiecutter.project_name}}"
+
+    if project_name_folder.exists() and project_name_folder.is_dir():
+        for item in project_name_folder.iterdir():
+            target = project_root / item.name
+            LOGGER.info("Moving %s to %s", item, target)
+            if item.is_dir():
+                shutil.move(str(item), str(target))
+            else:
+                shutil.move(str(item), str(target))
+
+    # input_dir = pathlib.Path("{{cookiecutter.project_name}}")
+
+    # for file in input_dir.glob("**/*"):
+    #     if file.is_file():
+    #         parent_folder = pathlib.Path(".").joinpath(file.parent)
+    #         parent_folder.mkdir(parents=True, exist_ok=True)
+    #         output_file = parent_folder.joinpath(file.name)
+    #         LOGGER.info("Copying %s to %s", file, output_file)
+    #         shutil.copy(file, output_file)
+    return True
+
+
+if __name__ == "__main__":
+    main()
